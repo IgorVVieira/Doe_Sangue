cd289ed2ac3970992a794cea8a4cca0071160061
igor.gutoo63@gmail.com <igor.gutoo63@gmail.com>
2022-03-01 09:56:20 -0300

    Altera porta da api

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,6 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
 });
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateDonorController } from "./controllers/CreateDonorController";
+import { IndexDonorController } from "./controllers/IndexDonorController";
 
 const routes = Router();
 
+routes.get('/donors', new IndexDonorController().handle);
 routes.post('/donors', new CreateDonorController().handle);
 
 export default routes;
